0fcf61476cf7c3c99011341a89f6ccd049e1b7a6
David Kovacs <david.kovacs@telekom.com>
2025-02-01 21:21:48 +0100

    add car counter

--- yolo_plays.py
+++ yolo_plays.py
@@ -7,9 +7,9 @@ while True:
     snapshot_path = input("Enter the path of the snapshot: ")
     results = model(snapshot_path)  # YOLO inference
     detections = results[0].boxes
-    car_detected = any(obj.cls == 2 for obj in detections)
+    car_detected = [obj for obj in detections if obj.cls == 2]
 
     if car_detected:
-        print("Car detected")
+        print(f"{len(car_detected)} car(s) detected")
     else:
         print("NO car detected")
