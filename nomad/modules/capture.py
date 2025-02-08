227d1a76c8661e809aee4103dd39023c895be64b
David Kovacs <david.kovacs@telekom.com>
2025-02-08 22:09:11 +0100

    add rtsp connection check

--- nomad/modules/capture.py
+++ nomad/modules/capture.py
@@ -1,4 +1,5 @@
 import os
+import socket
 from pathlib import Path
 from typing import Any
 
@@ -12,7 +13,33 @@ logger = get_logger(__name__)
 settings = get_rtsp_settings()
 
 
+def get_hostname(url: str) -> str:
+    """
+    Get the hostname from a URL.
+    """
+    return url.split("@")[1].split(":")[0]
+
+
+def check_url(host: str, port: int = 554, timeout: int = 5) -> bool:
+    """
+    Check if an RTSP connection can be established to the given host and port.
+    """
+    try:
+        print(host, port, timeout)
+        with socket.create_connection((host, port), timeout=timeout):
+            return True
+    except (socket.timeout, socket.error):
+        return False
+
+
 def capture_frame(file_path: str | Path, streaming: Any) -> bool:
+    """
+    Check RTSP stream connection, then capture
+    a single frame from an RTSP stream and save it to a file.
+    """
+    if not check_url(get_hostname(settings.url)):
+        logger.error("Failed to connect to the RTSP stream")
+        return False
     try:
         # Ensure the output directory exists
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
@@ -28,5 +55,4 @@ def capture_frame(file_path: str | Path, streaming: Any) -> bool:
 
     except ffmpeg._run.Error as err:
         logger.error(f"FFmpeg error: {err.stderr.decode()}")
-
         return False
