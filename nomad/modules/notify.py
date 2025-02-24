e8548f5bf74f0378bffd627dba98f1d96383a71b
David Kovacs <david.kovacs@telekom.com>
2025-02-24 21:54:57 +0100

    feat: :sparkles: introduce anti_flood in notify module

--- nomad/modules/notify.py
+++ nomad/modules/notify.py
@@ -12,12 +12,24 @@ bot_settings = get_telegram_bot_settings()
 bot = Bot(token=bot_settings.token)
 
 
+def anti_flood() -> bool:
+    """
+    Check the number of messages sent in the n most recent lines of the log file.
+    Returns True if the number of messages is more than 2, otherwise False.
+    """
+    most_recent_lines = 120  # new line every approx. 5 seconds; 5 * 120 = 600 seconds = 10 minutes
+    log_file = logger.handlers[0].baseFilename  # type: ignore
+    with open(log_file, "r") as f:
+        lines = f.readlines()[-most_recent_lines:]
+    messages = [line for line in lines if "Sending notifications to chat ids" in line]
+    return len(messages) >= 2
+
+
 async def send_message_and_snapshot(chat_id: int, text: str, snapshot_file: str | Path) -> None:
     """
-    Send a message and snapshot to a chat.
+    Send a snapshot to a chat.
     """
-    await bot.send_message(chat_id=chat_id, text=text)
-    await bot.send_photo(chat_id=chat_id, photo=snapshot_file)
+    await bot.send_photo(chat_id=chat_id, photo=snapshot_file, caption=text)
 
 
 def send_notification(
@@ -29,9 +41,13 @@ def send_notification(
     """
     Send a notification with the number of detected objects and a snapshot to all chat ids.
     """
+    if anti_flood():
+        logger.warning("ANTI-FLOOD triggered, skipping notification")
+        return
+
     message = f"{active_detection_type_name} DETECTED!"
     if not detection_positive:
         message = "aaaand is gone..."
+    logger.info(f"Sending notifications to chat ids: {bot_settings.chat_ids}")
     for chat_id in bot_settings.chat_ids:
-        logger.info(f"Sending a notification to chat ID: {chat_id}")
         event_loop.run_until_complete(send_message_and_snapshot(chat_id, message, snapshot_file))
